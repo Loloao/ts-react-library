0324692c9460a8b99625303135b5cb807eb1824c
liyang <l1312213036@outlook.com>
2020-09-18 20:02:54 +0800

    add build config

---
+++ vikingship/src/components/Menu/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import Menu, { MenuProps } from './menu'
+import SubMenu, { SubMenuProps } from './subMenu'
+import MenuItem, { MenuItemProps } from './menuItem'
+
+// 这里如果直接用 Menu.Item 的形式会报错，因为 Menu 类型上没有 Item 这个属性
+export type IMenuComponent = FC<MenuProps> & {
+  Item: FC<MenuItemProps>
+  SubMenu: FC<SubMenuProps>
+}
+
+const TransMenu = Menu as IMenuComponent
+
+TransMenu.Item = MenuItem
+TransMenu.SubMenu = SubMenu
+
+export default TransMenu
